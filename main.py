5fe12c14aad903d3c00bc7894077f956e3224a16
shbunyod2197 <shbunyod2197@gmail.com>
2025-12-12 16:02:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+# 1
+print(f"Kvadrati: {math.sqrt(144)}")
+# 2
+print(math.sqrt(225))
+# 3
+print(math.sqrt(169))
+# 4
+print(math.sqrt(81))
+# 5
+print(math.sqrt(400))
+# 6
+print(math.sqrt(121))
+# 7
+print(math.sqrt(49))
+# 8
+print(math.sqrt(625))
